e6f79c50acd7c1accf2088aa00e1d8587eabb941
kfjustis <kfjustis@gmail.com>
2022-04-17 17:26:56 -0500

    Add sentiment analysis functionality for strings/emojis

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ Final project for CSE 557A.
 
 App link: https://twitter-behavior-analyzer.onrender.com
 
-
 Credits
 ------
 
 Project template forked from: https://github.com/render-examples/express-hello-world
 
 Project setup: https://render.com/docs/deploy-node-express-app
+
+Sentiment analysis library: https://www.npmjs.com/package/sentiment
--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const path = require("path");
+const bodyparser = require("body-parser");
+const sentiment = require("sentiment");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/public'));
+var sent = new sentiment();
+var inputText = "empty";
 
+// Routes.
+app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/views/index.html');
+
+    // Set up callback for the analyze button.
+    //const btnAnalyze = document.querySelector("#btnAnalyze");
+    //const inputURL = document.querySelector("#basic-url");
+    //btnAnalyze.onclick = () => {
+    //  //const name = prompt('What is your name?');
+    //  //alert(`Hello ${name}, nice to see you!`);
+    //  //headingA.textContent = `Welcome ${name}`;
+    //  const textInput = inputUrl.textContent;
+    //  console.log("text: " + textInput);
+    //}
 });
-app.get('/results', function(request, response) {
-    response.sendFile(__dirname + '/views/results.html');
+app.post('/results', (req, res) => {
+    //res.sendFile(__dirname + '/views/results.html');
+    //var example = "Example text is very stupid.";
+    //var result = sent.analyze(example);
+    //console.log("ex: " + example);
+    //console.log(result);
+
+    var sentText = req.body.urlname;
+    var result = sent.analyze(sentText);
+    var score = result.score;
+    var comparative = result.comparative;
+    res.render('results',
+      {
+        sentText: sentText,
+        score: score,
+        comparative: comparative
+      });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
